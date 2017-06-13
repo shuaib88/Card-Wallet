21a81cd556f47f7a2d1a8c3f421d31ab256b22f2
Shuaib Ahmed <shuaib.sva@gmail.com>
2017-06-13 16:41:07 -0500

    more commits added ignore

--- Card Wallet/ViewController.swift
+++ Card Wallet/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var Cards: [UIImageView]!
+    
+    var snap: UISnapBehavior!
+    var animator: UIDynamicAnimator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(imageTapped))
+        
+        for card in Cards! {
+            card.addGestureRecognizer(tapGesture)
+            card.isUserInteractionEnabled = true
+        }
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        
+        self.view.layoutIfNeeded()
+        
+        
+    }
+    
+    func imageTapped(gesture: UIGestureRecognizer) {
+        if let imageView = gesture.view as? UIImageView {
+            print("Image Tapped")
+        }
     }
 
     override func didReceiveMemoryWarning() {
