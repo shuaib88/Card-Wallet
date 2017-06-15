91ce0d9cb99556544e26d84b92bf11b249b06f3b
Shuaib Ahmed <shuaib.sva@gmail.com>
2017-06-15 17:33:15 -0500

    made all the cards move horizontally

--- Card Wallet/ViewController.swift
+++ Card Wallet/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -15,24 +16,35 @@ class ViewController: UIViewController {
     
     var snap: UISnapBehavior!
     var animator: UIDynamicAnimator!
-
-    @IBOutlet weak var card: UIImageView!
-    
+    var push: UIPushBehavior!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        for card in Cards {
-            let tapGesture = UITapGestureRecognizer(target: self, action:#selector(imageTapped))
-            card.addGestureRecognizer(tapGesture)
-            card.isUserInteractionEnabled = true
-        }
-        
         animator = UIDynamicAnimator(referenceView: self.view)
         
         self.view.layoutIfNeeded()
+        
+        // added gesture recognizers
+        for card in Cards {
+            
+            // tap behaviors
+            let tapGesture = UITapGestureRecognizer(target: self, action:#selector(imageTapped))
+            card.addGestureRecognizer(tapGesture)
+            card.isUserInteractionEnabled = true
+            
+            var origPos = card.center
+            card.center = CGPoint(x: self.view.frame.width / 2, y: -card.frame.height)
+            
+            snap = UISnapBehavior(item: card, snapTo: origPos)
+            snap.damping = 0.3
+            animator.addBehavior(snap)
+            
+        }
+        
+        print("view loading")
     }
     
     func imageTapped(gesture: UIGestureRecognizer) {
@@ -40,7 +52,57 @@ class ViewController: UIViewController {
             print("Image Tapped")
         }
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            animator.removeAllBehaviors()
+            
+            push = UIPushBehavior(items: Cards, mode: UIPushBehaviorMode.instantaneous)
+            push.setAngle( CGFloat(Double.pi / 2) , magnitude: 5)
+            //            push.magnitude = 0.2
+            //            push.pushDirection = CGVector(dx: 0.0, dy: -1.0)
+            animator.addBehavior(push)
+            
+            /**
+            let card = Cards[1]
+            let location = touch.location(in: self.view)
+            push = UIPushBehavior(items: Cards, mode: UIPushBehaviorMode.continuous)
+            
+            
+            snap = UISnapBehavior(item: card as UIImageView!, snapTo: location)
+            snap.damping = 0.3
+            **/
 
+        }
+    }
+ 
+    /**
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+//            animator.removeAllBehaviors()
+            let card = Cards[1]
+            let location = touch.location(in: self.view)
+            
+            
+//            print(push.items)
+            print ("touches moved")
+            
+      **/
+            
+//            card.center = CGPoint(x: self.view.frame.width / 2, y: location.y)
+            
+            /**
+            snap = UISnapBehavior(item: card as UIImageView!, snapTo: location)
+            snap.damping = 0.3
+            animator.addBehavior(snap)
+ 
+ 
+        }
+    }
+ 
+ **/
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
